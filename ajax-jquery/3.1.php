a5cb97f28640fd670bdea01ede803c8c778f2cb5
naiara <npadulesir15dw@ikzubirimanteo.com>
2017-02-17 11:22:33 +0000

    listo

--- ajax-jquery/3.1.php
+++ ajax-jquery/3.1.php
@@ -2,17 +2,19 @@
 
 
     $tipos= $_POST['a'];
-
+    $todos = $_POST['b'];
     $jsonarchivo = file_get_contents("3.2.json");
     $jsondescodificar = json_decode($jsonarchivo, true);
     
     $jsonikasleak = $jsondescodificar["Ikasleak"];
     $jsonirakasleak = $jsondescodificar["Irakasleak"];
+    $jsonasignatura = $jsonasignatura["ikasgaia"];
     
     if($tipos == "ikasleak"){
         $arrayikasle = array();
         foreach ($jsonikasleak as $ikasle){
             array_push($arrayikasle, $ikasle);
+            
         }
         echo json_encode($arrayikasle, true);
     }elseif($tipos == "irakasleak"){
